3bf202aa495c8f43b2d16ab237c6d6f99e460277
fazionpietro <pietrofazion04@gmail.com>
2025-03-11 23:31:44 +0100

    login and signup api implementation

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import sqlite3 from 'sqlite3'
-import express from 'express' 
+import express from 'express'
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
+
 
 
 const app = express()
 app.use(express.json())
+const secretKey = 'triceratops-are-the-best-2025@1234'
 
 
 app.listen(5000, () => {
     console.log("Server starded at http://localhost:5000")
+    
 });
 
 
@@ -30,31 +35,123 @@ app.get("/api/prova", async (req, res)=> {
         res.header("Access-Control-Allow-Origin", "*");
         res.set({
             'Content-type': 'application/json', 
-
-        
         })
-        const data = await getExercises();
-        console.log(data)
-        res.status(200).json(data)
+        
+        res.status(200).json(all)
         
     } catch (err) {
         res.status(400).json({"error":err.message});
         console.error(err.message);
     }
-        
-
-        
-        
-    
+         
 })
 
-async function getExercises() {
+
+
+app.post('/signup', async (req, res)=> {
+    if(!checkEmail(req.query.email)) return res.status(401).json({"error":"invalid email"});
+    const email = req.query.email
+    const role = req.query.role
+    
+    try {
+        
+        const all = await getAllIdenty()
+        
+        
+        for(let i = 0; i < all.length; i++){
+            if(all[i].email === email){
+                
+                return res.status(401).json({"error":"email already exist"});;
+            }
+            if(await bcrypt.compare(req.query.psw, all[i].password)){
+                
+                return res.status(401).json({"error":"password already exist"});;
+            }
+            
+        }
+        const hashedPsw = await bcrypt.hash(req.query.psw,8)
+        await addUser(email, hashedPsw, role)
+        const token = jwt.sign({ id: email }, secretKey, { expiresIn: '1h' });
+        res.status(200).json({token})
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({"error":err.message});
+    }
+})
+
+
+app.post('/login', async (req, res)=> {
+    if(!checkEmail(req.query.email)) return res.status(401).json({"error":"invalid email"});
+    var token;
+    try {
+        
+        const all = await getAllIdenty()
+       
+        
+        for(let i = 0; i < all.length; i++){
+        
+            
+            if((all[i].email === req.query.email) && (await bcrypt.compare(req.query.psw, all[i].password))){
+                token = jwt.sign({ id: req.query.email }, secretKey, { expiresIn: '1h' });
+                return res.status(200).json({token});
+            }
+            
+        }
+        res.status(401).json({"error":"user dont exist"});
+
+        
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({"error":err.message});
+    }
+})
+
+
+
+
+
+
+
+
+
+
+function checkEmail(email){
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
+
+
+
+
+
+
+
+
+
+async function getAllIdenty() {
     return new Promise((resolve, reject) => {
-        db.all('SELECT * FROM prova', (err, rows) => {
+        db.all('SELECT email,password FROM users', (err, rows) => {
             if(err)
                 reject(err);
             else
                 resolve(rows);
+        });
+    });
+}
+
+
+function addUser(email,psw, role) {
+    return new Promise((resolve, reject) => {
+        db.run('INSERT INTO users(email, password, role) VALUES(?,?,?)', [email, psw, role], (err) => {
+
+            if(err)
+                reject(err);
+            else
+                resolve();
         });
     });
 }
