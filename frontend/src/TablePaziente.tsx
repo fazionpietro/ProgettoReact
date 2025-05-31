a27440ea4babc31bce6c435e464b187fda6e1e00
Litio-proto <proto@DESKTOP-Q8U223R.localdomain>
2025-05-31 15:09:55 +0200

    fix tabella dottori

---
+++ frontend/src/TablePaziente.tsx
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router"
+
+interface exercise{
+    id : number;
+    scheda_id: number;
+    esercizio_id: number;
+    serie: number;
+    ripetizioni: number;
+    nome_scheda: string;
+    note_scheda: string;
+}
+
+const TablePaziente: React.FC =()=>{
+
+    const [schede, setSchede] = useState<exercise[]>([]);
+    const {email} = useParams<{email?:string}>();
+    const navigate = useNavigate(); 
+
+    const getUserSchede = async () =>{
+        try {
+        await axios.get(`${import.meta.env.VITE_API_KEY}/schedeEserciziUtente?email=${email}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem(
+                "access_token"
+                )}`,
+            },
+        })
+        .then((resp) => {
+            if (resp.status === 200){ 
+                setSchede(resp.data.data);
+            }  
+        });
+        } catch (error) {
+            console.error(error);        
+        }
+    }
+
+    useEffect(()=>{
+        getUserSchede();
+    })
+
+    const schedeFiltrate = Array.from(
+        new Map(schede.map((sc)=>[sc.scheda_id,sc])).values()
+    );
+
+    const handleClick = (id: number) =>{
+        console.log("bottone: ",id)
+        if(!email){
+            return;
+        }
+        navigate(`/schedaDoc/${id}/${email}`);
+    }
+
+    return(
+        <div className="card">
+            <h2>Email Paziente: {email}</h2>
+            <table id='patienttable'>
+                <thead>
+                    <tr>
+                        <th>id scheda</th>
+                        <th>nome</th>
+                        <th>note</th>
+                        <th>visualizza scheda</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {schedeFiltrate.map((sc) => (
+                        <tr key={sc.scheda_id}>
+                            <td>{sc.scheda_id}</td>
+                            <td>{sc.nome_scheda}</td>
+                            <td>{sc.note_scheda}</td>
+                            <td><button onClick={()=>handleClick(sc.scheda_id)}>visualizza</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default TablePaziente;
